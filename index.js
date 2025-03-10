068a52907e3794103814a4691b29ef3017daec51
corene <corene.tabitha@student.moringaschool.com>
2025-03-10 09:44:01 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2
+let num3 = 104 
+let num4 = 10
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random()*10);
+
+const mod = num3 % num4;
+
+const max = Math.max(0, 5, 10, 15, 20);
